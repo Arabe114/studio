48636151d63742b76b169d0a1fe5950cc790d603
Emir Lima <emirlima114@gmail.com>
2025-09-07 12:55:24 +0000

    Nothing is saving in the database... Fix this right now... Task Board, K

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,7 +5,9 @@ let db: admin.firestore.Firestore;
 
 function getDb(): admin.firestore.Firestore {
     if (!admin.apps.length) {
-        admin.initializeApp();
+        admin.initializeApp({
+            projectId: "synergyhub-i1sba",
+        });
     }
     if (!db) {
         db = admin.firestore();
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,10 +5,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   "projectId": "synergyhub-i1sba",
   "appId": "1:1089312786800:web:1bd26c7fc9c80731ca67b5",
-  "storageBucket": "synergyhub-i1sba.firebasestorage.app",
+  "storageBucket": "synergyhub-i1sba.appspot.com",
   "apiKey": "AIzaSyCY9dJftZ2gBGMTpdCrrz6dXgsW4C4KBl4",
   "authDomain": "synergyhub-i1sba.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "1089312786800"
 };
 
