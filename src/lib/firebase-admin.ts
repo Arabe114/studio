4af38c9ec2a5a4764b729df42da3cc82a9aecfb7
Emir Lima <emirlima114@gmail.com>
2025-09-07 12:45:21 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,19 +1,12 @@
 import * as admin from 'firebase-admin';
 
-const firebaseConfig = {
-  "projectId": "synergyhub-i1sba",
-  "appId": "1:1089312786800:web:1bd26c7fc9c80731ca67b5",
-  "storageBucket": "synergyhub-i1sba.firebasestorage.app",
-  "apiKey": "AIzaSyCY9dJftZ2gBGMTpdCrrz6dXgsW4C4KBl4",
-  "authDomain": "synergyhub-i1sba.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1089312786800"
-};
+// This is the correct way to initialize the admin SDK in a server environment.
+// It will automatically use the project's service account credentials.
+const projectId = "synergyhub-i1sba";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    // Use projectId from your config
-    projectId: firebaseConfig.projectId,
+    projectId: projectId,
   });
 }
 
