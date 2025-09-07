327339b25c62acc7aa42afa7609f3a4f579ed5c7
Emir Lima <emirlima114@gmail.com>
2025-09-07 14:02:34 +0000

    Decrease the shake animation of the buttons of sidebar, it's so strong a

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,9 +95,9 @@ export default {
         },
         'shake': {
             '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
-            '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
-            '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
-            '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
+            '20%, 80%': { transform: 'translate3d(1px, 0, 0)' },
+            '30%, 50%, 70%': { transform: 'translate3d(-2px, 0, 0)' },
+            '40%, 60%': { transform: 'translate3d(2px, 0, 0)' },
         },
         'success-pop': {
           '0%': { transform: 'scale(1)' },
