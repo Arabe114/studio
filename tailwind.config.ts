e4512cdbce145c9cf562ed156292a4809ba9890e
Emir Lima <emirlima114@gmail.com>
2025-09-07 14:03:13 +0000

    Decrease more...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,8 +96,8 @@ export default {
         'shake': {
             '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
             '20%, 80%': { transform: 'translate3d(1px, 0, 0)' },
-            '30%, 50%, 70%': { transform: 'translate3d(-2px, 0, 0)' },
-            '40%, 60%': { transform: 'translate3d(2px, 0, 0)' },
+            '30%, 50%, 70%': { transform: 'translate3d(-1px, 0, 0)' },
+            '40%, 60%': { transform: 'translate3d(1px, 0, 0)' },
         },
         'success-pop': {
           '0%': { transform: 'scale(1)' },
